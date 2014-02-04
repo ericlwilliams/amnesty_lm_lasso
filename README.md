b53b029025cb3c26bc4891581dc13ab0bf1b42ae
Eric Williams <ericlwilliams@gmail.com>
2014-02-04 10:42:19 -0500

    category analysis

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-Testing linear/logistic regression models with regularization (lasso) for predicting Urgent Action follow-up
+# Amnesty International, Urgent Action (aiua) analyses
+## Testing linear/logistic regression models with regularization (lasso) for predicting Urgent Action *follow-up*
+---
+### aiua_categories.R: 
+- Summarizes urgent action *category* incidence with wordcloud and category incidence
+- i.e. shows overall frequencies of categories
+
+### aiua_logreg_fu_lasso.R
+- Given a category e.g. legal concern
+-- Builds Corpus + DocumentTermMatrix for all UAs of given category
+-- Runs logistic regression with 10-fold cross-validation to choose lambda (alpha=1 -> lasso) 
+--- test
--- aiua_logreg_fu_lasso.R
+++ aiua_logreg_fu_lasso.R
@@ -4,17 +4,63 @@ library(data.table)
 library(plyr)
 library(wordcloud)
 library(RColorBrewer)
-library("glmnet")
-library("boot")
-
+library(glmnet)
+library(boot)
 
 data.path <- "./data/aiua.csv"
+
+# Sparsity 
+sparsity = 0.99
+
+# Category dictionary
+# [1] "cat_abduction"               "cat_arbitrary_arrest"
+#  [3] "cat_arbitrary_detention"     "cat_conscientious_objector"
+#  [5] "cat_corporate_abuse"         "cat_death_penalty"
+#  [7] "cat_death_threat"            "cat_enforced_disappearance"
+#  [9] "cat_excessive_force"         "cat_extrajudicial_execution"
+# [11] "cat_fear_for_safety"         "cat_fear_of_torture"
+# [13] "cat_forced_eviction"         "cat_forced_return"
+# [15] "cat_freedom_of_expression"   "cat_harassment"
+# [17] "cat_harsh_prison_conditions" "cat_health_concern"
+# [19] "cat_house_arrest"            "cat_hunger_strike"
+# [21] "cat_ill_treatment"           "cat_imminent_execution"
+# [23] "cat_incommunicado_detention" "cat_legal_concern"
+# [25] "cat_lgbti_rights"            "cat_mass_arrest"
+# [27] "cat_medical_concern"         "cat_other"
+# [29] "cat_prisoner_of_conscience"  "cat_refugee_rights"
+# [31] "cat_risk_of_torture"         "cat_torture"
+# [33] "cat_unfair_trial"            "cat_unjust_arrest"
+# [35] "cat_unjust_imprisonment"     "cat_unlawful_killing"
+# [37] "cat_women_s_rights"          "cat_youth_children_s_rights"
+
+# category = "all"
+category = "cat_freedom_of_expression"
+save_path = paste("./figures/",category,sep="")
+
+dir.create(save_path, showWarnings = FALSE)
+
+
+ua.dt <- read.csv(data.path,sep=",",stringsAsFactors=FALSE)
+
+if(category == "all"){
+	cat_ind <- -1
+}else{
+	cats <- seq(6,43)
+	names(cats) <- colnames(ua.dt)[cats]
+	cat_ind <- cats[category]
+}
+
+
 
 # split data up into wfu (with follow-up) and wofu (without-followup)
 # use random 75% of entries of each 
 
-# build text corpus
-ua.dt <- read.csv(data.path,sep=",",stringsAsFactors=FALSE)
+
+if(cat_ind>=0){
+	ua.dt <- ua.dt[which(ua.dt[,cat_ind]==1),]
+}
+# skim for category
+
 ua.dt <- data.table(ua.dt)
 
 ua.dt<-ua.dt[,list(id=data_id,body=body,status=action,idate=issue_date)]
@@ -33,21 +79,31 @@ ua.dt<-merge(ua.dt,id.counts[2:nrow(id.counts),],by="id")
 ua.dt<-ua.dt[!duplicated(ua.dt$id),]
 
 
+word.freq <- sapply(ua.dt$body, function(s) nchar(s))
+body.lengths <- data.frame(freq=word.freq)
 
-body.lengths <- data.frame(freq=sapply(ua.dt$body, function(s) nchar(s)))
+# default body entry very common 
+# "urgent  action uanetwork office aiusa 600 pennsylvania ave se, washington dc 20003 t. 202.544.0200 f. 202.675.8566 e. uan@aiusa.org amnestyusa.org/urgent/ "
+#  to find and optionally remove index 155
+# print(ua.dt$body[which(body.lengths$freq==155)])
+
 ggplot(body.lengths,aes(x=log(freq))) +
 geom_histogram(binwidth=.1)
-ggsave("./figures/h_body_nchar.pdf")
+ggsave(paste(save_path,"h_body_nchar.pdf",sep="/"))
+
+
 # Simplify to random selection of 500 UAs
-indices <- sample(1:nrow(ua.dt), 750)
-ua.dt <- ua.dt[indices,]
 
-body.lengths <- data.frame(freq=sapply(ua.dt$body, function(s) nchar(s)))
+
+# indices <- sample(1:nrow(ua.dt), 2000)
+# ua.dt <- ua.dt[indices,]
+
+body.lengths <- data.frame(freq=word.freq)
 ggplot(body.lengths,aes(x=log(freq))) +
 geom_histogram(binwidth=.1)
-ggsave("./figures/h_trim_body_nchar.pdf")
+ggsave(paste(save_path,"h_trim_body_nchar.pdf",sep="/"))
 
-browser()
+
 documents <- data.frame(Text = ua.dt$body)
 
 row.names(documents) <- 1:nrow(documents)
@@ -55,11 +111,23 @@ corpus <- Corpus(DataframeSource(documents))
 corpus <- tm_map(corpus, tolower)
 corpus <- tm_map(corpus, stripWhitespace)
 corpus <- tm_map(corpus, removeNumbers)
-corpus <- tm_map(corpus, removeWords, stopwords('english'))
+corpus <- tm_map(corpus, removeWords, c(stopwords('english'),"amnesty","international","information","human","rights","uan@aiusa.org","(m)","amnestyusa.org/urgent/","()","(),"))
 
 aiua.dtm <- DocumentTermMatrix(corpus)
+# testing sparcity
+# Removes terms which have at least sparse fraction of sparsity 
+# (eg. sparse = 0.9 <- Remove terms that *aren't* in 90% or more of documents)
+
+
+aiua.dtm <- removeSparseTerms(aiua.dtm,sparse=sparsity)
 
 aiua.dtm <- as.matrix(aiua.dtm)
+
+# browser()
+# # Only keep 100 top terms
+# aiua.dtm <- sort(rowSums(aiua.dtm),decreasing=TRUE)
+# aiua.dtm <- head(aiua.dtm,100)
+# browser()
 
 # For word cloud
 v <- sort(rowSums(t(aiua.dtm)),decreasing=TRUE)
@@ -80,18 +148,25 @@ y <- ua.dt$wfu
 
 # Does k-fold cross-validation for glmnet, produces a plot, and returns a value for lambda
 
-cvfit <- cv.glmnet(x,y,nfolds=10,family="binomial",type.measure="class")
+cvfit <- cv.glmnet(x,y,nfolds=nrow(x),family="binomial",type.measure="class")
 
-pdf("./figures/aiua_follow_up_misclass_v_lambda.pdf")
+pdf(paste(save_path,"aiua_follow_up_misclass_v_lambda.pdf",sep="/"))
 plot(cvfit)
+dev.off()
+
+
+
+glmfit.test <- cvfit$glmnet.fit
+
+pdf(paste(save_path,"aiua_follow_up_coef_v_lambda.pdf",sep="/"))
+plot(glmfit.test,xvar="lambda")
 dev.off()
 
 best.lambda <- cvfit$lambda.min
 
-# glmfit.test <- cvfit.glmnet.fit1
+fits <- glmnet(x,y,family="binomial",lambda=best.lambda)
 
-pdf("./figures/aiua_follow_up_coef_v_lambda.pdf")
-plot(glmfit.test,xvar="lambda")
+
 # x=matrix(rnorm(100*20),100,20)
 # y=rnorm(100)
 # g2=sample(1:2,100,replace=TRUE)
